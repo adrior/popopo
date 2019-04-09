ce26b8babe99b9d4897b098d3449d16464115370
Anton Shein <adrior@yandex-team.ru>
2019-04-10 00:14:36 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import POLL from "./poll.json";
 
 class App extends Component {
+  state = {
+    currentQuestion: null
+  };
+
+  next = () => {
+    let cq = this.state.currentQuestion;
+    if (cq === null) {
+      cq = 0;
+    } else {
+      cq++;
+    }
+    this.setState({ currentQuestion: cq });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.currentQuestion === null && (
+          <div className="App__intro">
+            <h2>{POLL.intro.title}</h2>
+            <div onClick={this.next}>{POLL.intro.descr}</div>
+          </div>
+        )}
+        {this.state.currentQuestion !== null &&
+          this.state.currentQuestion < POLL.questions.length && (
+            <div className="App__quesion">
+              <h2>{POLL.questions[this.state.currentQuestion].descr}</h2>
+              <div>
+                {POLL.questions[this.state.currentQuestion].answers.map(ans => (
+                  <div onClick={this.next}>{ans.descr}</div>
+                ))}
+              </div>
+            </div>
+          )}
       </div>
     );
   }
