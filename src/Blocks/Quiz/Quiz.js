b98435c7758eb1bbba6732463d9bd4b358dd9b3a
Anton Shein <adrior@yandex-team.ru>
2019-05-10 14:40:26 +0300

    Before Firebase

--- src/Blocks/Quiz/Quiz.js
+++ src/Blocks/Quiz/Quiz.js
@@ -9,35 +9,63 @@ export default class Quiz extends React.Component {
     score: {}
   };
 
+  checkResults_numberOfCorrect = () => {
+    let score = this.state.score.count;
+    let results = this.props.json.results;
+    let nearest = null;
+    let i = 0;
+    while (!(nearest = results[score]) && i < 100) {
+      score--;
+      i++;
+    }
+    this.setState({ result: nearest });
+  };
+
+  checkResults_mostRelevant = () => {
+    let score = this.state.score;
+    let maxKey = Object.keys(score).reduce((a, b) =>
+      score[a] > score[b] ? a : b
+    );
+    this.setState({ result: this.props.json.results[maxKey] });
+  };
+
   checkResults = () => {
-    let strategy = this.props.json.resutlsStrategy || "grades";
-    if (strategy === "chooseFromList") {
-      let score = this.state.score;
-      let maxKey = Object.keys(score).reduce((a, b) =>
-        score[a] > score[b] ? a : b
-      );
-      console.log(maxKey, this.props.json.results[maxKey]);
-    } else if (strategy === "grades") {
-      console.error(`There is no «${strategy}» strategy defined`);
-    } else {
-      console.error(`There is no «${strategy}» strategy defined`);
+    let methodName = "checkResults_" + this.props.json.config.strategy;
+    this[methodName]();
+  };
+
+  handleNext_numberOfCorrect = score => {
+    let prevScore = { ...this.state.score };
+    if (prevScore.count === undefined) {
+      prevScore.count = 0;
+    }
+    if (score) {
+      prevScore.count += score;
+      this.setState({ score: prevScore });
     }
   };
 
-  handleNext = score => {
+  handleNext_mostRelevant = score => {
     if (score) {
-      let prevScore = this.state.score;
+      let prevScore = { ...this.state.score };
       for (let i in score) {
         if (!prevScore[i]) prevScore[i] = 0;
         prevScore[i] += score[i];
       }
-      this.setState({ score: score });
+      this.setState({ score: prevScore });
     }
-    if (this.state.currentQuestion === this.props.json.questions.length) {
-      this.checkResults(this.state.score);
-    } else {
-      this.setState({ currentQuestion: this.state.currentQuestion + 1 });
+  };
+
+  handleNext = score => {
+    let currentQuestion = this.state.currentQuestion;
+    if (currentQuestion !== null) {
+      let methodName = "handleNext_" + this.props.json.config.strategy;
+      this[methodName](score);
+      if (currentQuestion + 1 > this.props.json.questions.length) {
+        setTimeout(this.checkResults, 0);
+      }
     }
+    this.setState({ currentQuestion: currentQuestion + 1 });
   };
 
   render() {
@@ -52,7 +80,9 @@ export default class Quiz extends React.Component {
             handleAction={this.handleNext}
           />
         );
-      } else {
+      } else if (
+        this.state.currentQuestion <= this.props.json.questions.length
+      ) {
         let question = this.props.json.questions[
           this.state.currentQuestion - 1
         ];
@@ -68,6 +98,8 @@ export default class Quiz extends React.Component {
             }
           />
         );
+      } else if (this.state.result !== undefined) {
+        content = <div>{this.state.result.title}</div>;
       }
     }
     return (
